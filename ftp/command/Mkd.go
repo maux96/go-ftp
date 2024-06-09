a2773b08a9efaa4bd8583ee6dae6d27d02496fe5
Mauricio Mahmud Sanchez <mauxms1996@gmail.com>
2024-06-09 22:17:20 +0200

    fix: MKD, white spaces

--- ftp/command/Mkd.go
+++ ftp/command/Mkd.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -9,8 +10,10 @@ type MkdCommand BaseCommand
 
 func (cmd MkdCommand) Action(ctx *ConnCtx) error {
 	/* TODO verifiy when exist a file named like the directory */
-	parentPath := strings.Join(cmd.Args[:len(cmd.Args)-1], " ")
+
 	path := strings.Join(cmd.Args, " ")
+	dirName := filepath.Base(path)
+	parentPath := strings.Join(cmd.Args, " ")[:len(path)-len(dirName)]
 
 	_, exists := ctx.GetRealPath(parentPath)
 	if !exists {
